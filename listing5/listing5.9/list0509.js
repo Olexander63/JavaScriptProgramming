64004bd73b5b3117476bc30f7c5650d9d2738c5b
Oleksandr Omelchenko <olexander63@gmail.com>
2022-01-05 20:54:28 +0200

    listing5,9

---
+++ listing5/listing5.9/list0509.js
@@ -0,0 +1,9 @@
+let A = [1, 2, 3, 4, 5];
+document.write("A=[" + A + "]<br>");
+let B = ["a", "b", "c"];
+B = A;
+document.write("После присваивания <code>B=A</code>:<br>");
+document.write("B=[" + B + "]<br>");
+A[0] = 100;
+document.write("После присваивания <code>A[0]=100</code>:<br>");
+document.write("B=[" + B + "]<br>");

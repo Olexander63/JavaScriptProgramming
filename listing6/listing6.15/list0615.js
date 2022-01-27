669d180dfcd23f0a637be28be5a911c3d5fa6d71
Oleksandr Omelchenko <olexander63@gmail.com>
2022-01-27 08:15:55 +0200

    list6.15.html

---
+++ listing6/listing6.15/list0615.js
@@ -0,0 +1,14 @@
+﻿let myTrue = Boolean(true)
+let myFalse = Boolean(false)
+if (myTrue) {
+    document.write("Обьект myTrue<br>")
+}
+if (myTrue === true) {
+    document.write("Снова обьект myTrue<br>")
+}
+if(myFalse){
+    document.write("Обьект myFalse<br>")
+}
+if (myFalse === false) {
+    document.write("Снова обьект myFalse")
+}
